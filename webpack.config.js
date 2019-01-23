ac2fa2bd5520e9939b2ba60599cd68526b589687
Thomas Juhnke <tommy@van-tomas.de>
2019-01-23 19:03:02 +0100

    Add basic webpack config with adjusted src + dist paths

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = [{
+    entry: './source-assets/styles/main.scss',
+    output: {
+        // This is necessary for webpack to compile
+        // But we never use style-bundle.js
+        filename: './dist/scripts/style-bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            // TODO: change file name to "main.css"
+                            name: './dist/styles/main-mdc.css',
+                        },
+                    },
+                    {loader: 'extract-loader'},
+                    {loader: 'css-loader'},
+                    {loader: 'sass-loader'},
+                ]
+            }
+        ]
+    },
+}];
