b5963e2ba208969ada4314c58cd89e9ae0b8862b
Thomas Juhnke <tommy@van-tomas.de>
2019-02-24 20:25:55 +0100

    Streamline vcard component

--- src/components/vcard/index.js
+++ src/components/vcard/index.js
@@ -6,14 +6,14 @@ export default () => (
   <section className="section--center mdc-layout-grid vcard">
     <div className="mdc-layout-grid__inner">
       <div
-        className="mdc-layout-grid__cell mdc-layout-grid__cell--span-4 mdc-layout-grid__cell--span-2-tablet mdc-layout-grid__cell--span-4-phone mdc-layout-grid__cell--align-middle mdl-typography--text-center vcard__avatar-container">
+        className="mdc-layout-grid__cell mdc-layout-grid__cell--span-4 mdc-layout-grid__cell--span-2-tablet mdc-layout-grid__cell--span-4-phone mdc-layout-grid__cell--align-middle">
         <Portrait />
       </div>
       <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-8 mdc-layout-grid__cell--span-6-tablet mdc-layout-grid__cell--span-4-phone mdc-layout-grid__cell--align-middle">
-        <h1 className="vcard__header mdl-typography--text-uppercase">
-          <small className="vcard__header--small">Thomas</small><br/>Juhnke<br/>
-          <small className="vcard__header--small">Software&shy;entwickler</small>
+        <h1 className="vcard__header mdc-typography--headline3">
+          <small className="vcard__header--small mdc-typography--headline4">Thomas</small><br/>Juhnke
         </h1>
+        <h2 className="vcard__header--small mdc-typography--headline4">Software&shy;entwickler</h2>
       </div>
     </div>
   </section>
