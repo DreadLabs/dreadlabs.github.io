fe4516ad6cd60275027f10c962962160c9c600e9
Thomas Juhnke <tommy@van-tomas.de>
2019-02-24 20:00:01 +0100

    Remove ported artifacts from old sources / assets files

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,4 @@
-/**
+/*!
  *
  *  Web Starter Kit
  *  Copyright 2015 Google Inc. All rights reserved.
@@ -24,96 +24,53 @@
   // and that the current page is accessed from a secure origin. Using a
   // service worker from an insecure origin will trigger JS console errors. See
   // http://www.chromium.org/Home/chromium-security/prefer-secure-origins-for-powerful-new-features
-  const isLocalhost = Boolean(window.location.hostname === 'localhost' ||
+  var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
       // [::1] is the IPv6 localhost address.
       window.location.hostname === '[::1]' ||
       // 127.0.0.1/8 is considered localhost for IPv4.
       window.location.hostname.match(
-          /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
       )
-  );
+    );
 
   if ('serviceWorker' in navigator &&
       (window.location.protocol === 'https:' || isLocalhost)) {
     navigator.serviceWorker.register('/service-worker.js', {scope: '/'})
-        .then(function(registration) {
-          // updatefound is fired if service-worker.js changes.
-          registration.onupdatefound = function() {
-            // updatefound is also fired the very first time the SW is
-            // installed, and there's no need to prompt for a reload at that
-            // point.
-            // So check here to see if the page is already controlled,
-            // i.e. whether there's an existing service worker.
-            if (navigator.serviceWorker.controller) {
-              // The updatefound event implies that registration.installing
-              // is set:
-              // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
-              const installingWorker = registration.installing;
+    .then(function(registration) {
+      // updatefound is fired if service-worker.js changes.
+      registration.onupdatefound = function() {
+        // updatefound is also fired the very first time the SW is installed,
+        // and there's no need to prompt for a reload at that point.
+        // So check here to see if the page is already controlled,
+        // i.e. whether there's an existing service worker.
+        if (navigator.serviceWorker.controller) {
+          // The updatefound event implies that registration.installing is set:
+          // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
+          var installingWorker = registration.installing;
 
-              installingWorker.onstatechange = function() {
-                switch (installingWorker.state) {
-                  case 'installed':
-                    // At this point, the old content will have been purged
-                    // and the fresh content will have been added to the cache.
-                    // It's the perfect time to display a "New content is
-                    // available; please refresh." message in the page's
-                    // interface.
-                    break;
+          installingWorker.onstatechange = function() {
+            switch (installingWorker.state) {
+              case 'installed':
+                // At this point, the old content will have been purged and the
+                // fresh content will have been added to the cache.
+                // It's the perfect time to display a "New content is
+                // available; please refresh." message in the page's interface.
+                break;
 
-                  case 'redundant':
-                    throw new Error('The installing ' +
+              case 'redundant':
+                throw new Error('The installing ' +
                                 'service worker became redundant.');
 
-                  default:
-                    // Ignore
-                }
-              };
+              default:
+                // Ignore
             }
           };
-        }).catch(function(e) {
-          console.error('Error during service worker registration:', e);
-        });
+        }
+      };
+    }).catch(function(e) {
+      console.error('Error during service worker registration:', e);
+    });
   }
 
   // Your custom JavaScript goes here
-  const d = document.querySelector('.mdl-layout');
-  const linksSelector = '.mdl-layout__drawer .mdl-navigation__link';
-  const links = document.querySelectorAll(linksSelector);
-
-  [].map.call(links, function(link) {
-    link.addEventListener('click', function() {
-      d.MaterialLayout.toggleDrawer();
-    });
-  });
-
-  let tagState = 'off';
-  const tags = document.querySelectorAll('.resume__position-tags');
-  const tagToggle = document
-      .querySelector('ul[for="resume-tags-toggle__button"] li');
-  const tagToggleState = tagToggle.querySelector('.resume-tags-toggle__state');
-
-  tagToggle.addEventListener('click', function() {
-    [].map.call(tags, function(tag) {
-      tag.classList.toggle('resume__position-tags--on');
-    });
-    if (tagState === 'off') {
-      tagToggleState.innerHTML = 'deaktivieren';
-      tagState = 'on';
-    } else if (tagState === 'on') {
-      tagToggleState.innerHTML = 'aktivieren';
-      tagState = 'off';
-    }
-  });
-
-  /* eslint-disable */
-
-  // responsive images
-  require('../images/Thomas-Juhnke_portrait-business.jpg?name=[name]-[width].[ext],sizes[]=271,sizes[]=542,sizes[]=185,sizes[]=370');
-  require('../images/Thomas-Juhnke_portrait-casual.jpg?name=[name]-[width].[ext],sizes[]=271,sizes[]=542,sizes[]=185,sizes[]=370');
-
-  require('../images/network/christophe-maurice.jpg?outputPath=images/network/,name=[name]-[width].[ext],sizes[]=480,sizes[]=960,sizes[]=396,sizes[]=792');
-  require('../images/network/michael-gerstmann.jpg?outputPath=images/network/,name=[name]-[width].[ext],sizes[]=480,sizes[]=960,sizes[]=396,sizes[]=792');
-  require('../images/network/robert-schoenthal.jpg?outputPath=images/network/,name=[name]-[width].[ext],sizes[]=480,sizes[]=960,sizes[]=396,sizes[]=792');
-
-  /* eslint-enable */
 })();
