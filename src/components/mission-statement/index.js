1fa30c919f722084b47b2173b1425f7b440041b0
Thomas Juhnke <tommy@van-tomas.de>
2019-03-03 12:27:24 +0100

    Optimize whitespace handling in mission-statement

--- src/components/mission-statement/index.js
+++ src/components/mission-statement/index.js
@@ -10,14 +10,14 @@ export default () => (
       <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
         <h2 className="mdc-typography--headline3">Leitbild</h2>
 
-        <p>Ich entwickle <strong>Software nach Ihren Wünschen</strong> mit hohen Anforderungen an
-          <strong>Qualität, Sicherheit und Zuverlässigkeit</strong>, testgetrieben und unter
-          Berücksichtigung Ihrer Geschäftsdomäne in Anlehnung an Domain Driven
-          Design-Praktiken.</p>
+        <p>Ich entwickle <strong>Software nach Ihren Wünschen</strong> mit hohen
+          Anforderungen an <strong>Qualität, Sicherheit und Zuverlässigkeit</strong>,
+          testgetrieben und unter Berücksichtigung Ihrer Geschäftsdomäne in Anlehnung
+          an Domain Driven Design-Praktiken.</p>
 
-        <p>Die <strong>Pflege und Weiterentwicklung Ihrer bestehenden Softwarelösungen</strong> sind
-          für mich ebenso bedeutsam wie die Integration von <strong>neuen Technologien, wenn
-            es sinnvoll ist</strong> und es die Anforderungen zulassen.</p>
+        <p>Die <strong>Pflege und Weiterentwicklung Ihrer bestehenden Softwarelösungen</strong>
+          sind für mich ebenso bedeutsam wie die Integration von <strong>neuen Technologien,
+          wenn es sinnvoll ist</strong> und es die Anforderungen zulassen.</p>
       </div>
     </div>
   </CenteredSection>
