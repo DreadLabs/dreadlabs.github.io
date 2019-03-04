84ecef7b406c65c0077b331c0908f17e6512e061
Thomas Juhnke <tommy@van-tomas.de>
2019-03-04 22:42:01 +0100

    Add story component

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import MissionStatement from "../components/mission-statement"
 import Network from "../components/network"
 import Quote from "../components/quote"
 import Resume from "../components/resume"
+import Story from "../components/story"
 import Vcard from "../components/vcard"
 
 class Index extends React.Component {
@@ -22,43 +23,34 @@ class Index extends React.Component {
 
         <MissionStatement />
 
-        <Centered additionalClasses="mdc-layout-grid">
-          <div className="mdc-layout-grid__inner">
-            <h3 className="mdc-typography--headline4 mdc-layout-grid__cell mdc-layout-grid__cell--span-4">Mein Angebot</h3>
-            <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-8">
-              <p>Ich unterstütze Sie bei …</p>
-              <ul>
-                <li>der Konzeption &amp; Planung von kleinen bis mittelgroßen Softwareprojekten</li>
-                <li>der Softwareentwicklung und -architektur mit agilen Vorgehensmodellen, auch im Team</li>
-                <li>der Pflege und Weiterentwicklung Ihrer bestehenden Softwarelösungen</li>
-                <li>der Verbesserung der Softwarequalität und -stabilität</li>
-              </ul>
-            </div>
-          </div>
-        </Centered>
+        <Story title="Mein Angebot">
+          <p>Ich unterstütze Sie bei …</p>
+          <ul>
+            <li>der Konzeption &amp; Planung von kleinen bis mittelgroßen Softwareprojekten</li>
+            <li>der Softwareentwicklung und -architektur mit agilen Vorgehensmodellen, auch im Team</li>
+            <li>der Pflege und Weiterentwicklung Ihrer bestehenden Softwarelösungen</li>
+            <li>der Verbesserung der Softwarequalität und -stabilität</li>
+          </ul>
+        </Story>
 
+        <Story id="about-me" title="Über mich">
+          <p>Bereits seit 1999 entwickle ich Software in verschiedenen Programmiersprachen.
+            Durch mein Studium der Informatik und Wirtschaftsinformatik und meine <strong>langjährige
+              Erfahrung</strong> in Unternehmen unterschiedlicher Größe und Aufgabengebieten decke
+            ich die umfangreiche Palette der Anforderungen an einen Softwareentwickler sehr
+            gut ab:
+          </p>
 
-        <Centered id="about-me" additionalClasses="mdc-layout-grid">
-          <div className="mdc-layout-grid__inner">
-            <h3 className="mdc-typography--headline4 mdc-layout-grid__cell mdc-layout-grid__cell--span-4">Über mich</h3>
+          <ul>
+            <li>technische Konzeption, Planung und Kommunikation mit allen Interessensgruppen</li>
+            <li>Continuous Integration und Continous Delivery</li>
+            <li>Profiling und statische Codeanalyse</li>
+            <li>infrastrukturnahe Tätigkeiten ("DevOps")</li>
+          </ul>
+        </Story>
 
-            <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-8">
-              <p>Bereits seit 1999 entwickle ich Software in verschiedenen Programmiersprachen.
-                Durch mein Studium der Informatik und Wirtschaftsinformatik und meine <strong>langjährige
-                  Erfahrung</strong> in Unternehmen unterschiedlicher Größe und Aufgabengebieten decke
-                ich die umfangreiche Palette der Anforderungen an einen Softwareentwickler sehr
-                gut ab:</p>
-
-              <ul>
-                <li>technische Konzeption, Planung und Kommunikation mit allen Interessensgruppen</li>
-                <li>Continuous Integration und Continous Delivery</li>
-                <li>Profiling und statische Codeanalyse</li>
-                <li>infrastrukturnahe Tätigkeiten ("DevOps")</li>
-              </ul>
-            </div>
-
-            <h4 className="mdc-typography--headline5 mdc-layout-grid__cell mdc-layout-grid__cell--span-12">Berufserfahrung</h4>
-          </div>
+        <Centered id="network" additionalClasses="section__network mdc-layout-grid">
+          <h4 className="mdc-typography--headline5 mdc-layout-grid__cell mdc-layout-grid__cell--span-12">Berufserfahrung</h4>
         </Centered>
 
         <Resume/>
