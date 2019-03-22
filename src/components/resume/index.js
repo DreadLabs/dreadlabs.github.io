66f23f86bcaaf9c02af7b80aad855be0c080fb74
Thomas Juhnke <tommy@van-tomas.de>
2019-03-22 12:21:14 +0100

    Update resume

--- src/components/resume/index.js
+++ src/components/resume/index.js
@@ -38,15 +38,39 @@ export default() => (
 
             <Position headline="Postcon Deutsch­land B.V.&nbsp;&amp;&nbsp;Co.&nbsp;KG" date="seit 2017" location="Berlin">
               <ul>
-                <li>Entwicklung in einer verteilten Systemarchitektur</li>
-                <li>Verwendung des Message queuing service RabbitMQ für den Nachrichtenaustausch</li>
-                <li>Implementierung von REST APIs, u.a. als Schnittstelle für Frankiermaschinen von Pitney Bowes und Neopost</li>
-                <li>Optimierung des Rechnungssystems im firmeneigenen ERP-System gemäß der <abbr title="Grundsätze ordnungsmäßiger Buchführung">GoB</abbr></li>
-                <li>Datenaustausch und -verarbeitung von SAP Schnittstellendaten</li>
+                <li>Implementierung des Hexagonalen Architekturmusters (Ports und Adapter)</li>
+                <li>Dokumentation von Softwarearchitekturen mit dem <a href="https://arc42.de/">arc42 Template</a></li>
                 <li>Enge Zusammenarbeit mit anderen Teammitgliedern für die Planung, das Design und die Entwicklung von robusten Softwarelösungen</li>
+                <li>Datenaustausch und -verarbeitung von SAP Schnittstellendaten</li>
+                <li>Optimierung des Rechnungssystems im firmeneigenen ERP-System gemäß der <abbr title="Grundsätze ordnungsmäßiger Buchführung">GoB</abbr></li>
+                <li>Implementierung von REST APIs, u.a. als Schnittstelle für Frankiermaschinen von Pitney Bowes und Neopost</li>
+                <li>Verwendung des Message queuing service RabbitMQ für den Nachrichtenaustausch</li>
+                <li>Entwicklung in einer verteilten Systemarchitektur</li>
               </ul>
 
-              <Tags tags={["OOP", "OOA & OOD", "TDD", "MQS", "CQRS", "Event Sourcing", "REST", "Scrum", "Event Storming", "PHP", "SQL", "JavaScript", "Symfony 2", "AngularJS", "MariaDB", "MongoDB", "RabbitMQ", "Docker", "Jenkins"]} />
+              <Tags tags={[
+                "OOP",
+                "OOA & OOD",
+                "TDD",
+                "MQS",
+                "CQRS",
+                "Event Sourcing",
+                "REST",
+                "Scrum",
+                "Event Storming",
+                "PHP",
+                "SQL",
+                "JavaScript",
+                "Symfony 2",
+                "AngularJS",
+                "MariaDB",
+                "MongoDB",
+                "RabbitMQ",
+                "Docker",
+                "Jenkins",
+                "arc42",
+                "Hexagonale Architektur",
+              ]} />
             </Position>
 
             <Position headline="McFIT Global&nbsp;Group&nbsp;GmbH" date="2016" location="Berlin">
@@ -58,7 +82,25 @@ export default() => (
                 <li>Implementierung von Docker als Laufzeitumgebung für lokale Entwicklungsumgebungen</li>
               </ul>
 
-              <Tags tags={["OOP", "TDD", "REST", "Scrum", "PHP", "Groovy", "Python", "SQL", "JavaScript", "Symfony 2", "node.js", "Express.js", "MariaDB", "CouchDB", "Ansible", "Docker", "Jenkins"]} />
+              <Tags tags={[
+                "OOP",
+                "TDD",
+                "REST",
+                "Scrum",
+                "PHP",
+                "Groovy",
+                "Python",
+                "SQL",
+                "JavaScript",
+                "Symfony 2",
+                "node.js",
+                "Express.js",
+                "MariaDB",
+                "CouchDB",
+                "Ansible",
+                "Docker",
+                "Jenkins",
+              ]} />
             </Position>
 
             <Position headline="bytepark GmbH" date="2013 - 2016" location="Berlin">
@@ -70,7 +112,20 @@ export default() => (
                 <li>Teamleitung und -koordination</li>
               </ul>
 
-              <Tags tags={["OOP", "REST", "JCR", "Kanban", "PHP", "Symfony 2", "Ansible", "MariaDB", "nginx", "Varnish", "Vagrant", "Jenkins"]} />
+              <Tags tags={[
+                "OOP",
+                "REST",
+                "JCR",
+                "Kanban",
+                "PHP",
+                "Symfony 2",
+                "Ansible",
+                "MariaDB",
+                "nginx",
+                "Varnish",
+                "Vagrant",
+                "Jenkins",
+              ]} />
             </Position>
 
             <Position headline="K - New Media GmbH&nbsp;&amp;&nbsp;Co.&nbsp;KG" date="2012 - 2013" location="Berlin">
@@ -79,7 +134,13 @@ export default() => (
                 <li>Rückportierung von Funktionen des aktuellen TYPO3-Kerns in den archaischen MB3-Kern</li>
               </ul>
 
-              <Tags tags={["OOP", "Kanban", "PHP", "Vagrant", "Jira"]}/>
+              <Tags tags={[
+                "OOP",
+                "Kanban",
+                "PHP",
+                "Vagrant",
+                "Jira",
+              ]}/>
             </Position>
 
             <Position headline="profi.it internet&nbsp;technology" date="2007 - 2012" location="Lana, Italien">
@@ -94,7 +155,17 @@ export default() => (
                 <li>Einführung der Versionsverwaltungssysteme git und svn in den Entwicklungsprozess</li>
               </ul>
 
-              <Tags tags={["OOP", "SOAP", "Kanban", "PHP", "JavaScript", "symfony 1.x", "TYPO3", "Extbase", "Debian"]} />
+              <Tags tags={[
+                "OOP",
+                "SOAP",
+                "Kanban",
+                "PHP",
+                "JavaScript",
+                "symfony 1.x",
+                "TYPO3",
+                "Extbase",
+                "Debian",
+              ]} />
             </Position>
 
             <Position headline="Haus&nbsp;Neuer&nbsp;Medien GmbH" date="2004 - 2006" location="Greifswald">
@@ -106,7 +177,13 @@ export default() => (
                 <li>Entwicklung und Debugging der JavaScript RTE-Komponente für das firmeneigene CMS</li>
               </ul>
 
-              <Tags tags={["OOP", "PHP", "JavaScript", "TYPO3", "Debian"]} />
+              <Tags tags={[
+                "OOP",
+                "PHP",
+                "JavaScript",
+                "TYPO3",
+                "Debian",
+              ]} />
             </Position>
 
             <Position headline="Contas&nbsp;GmbH / geist-nord&nbsp;gmbh" date="2001" location="Greifswald">
@@ -115,7 +192,12 @@ export default() => (
                 <li>Programmierung einer Anwendung zur Berechnung der Wirtschaftlichkeit von Kreditsicherheiten</li>
               </ul>
 
-              <Tags tags={["OOP", "Delphi", "SQL", "Crystal Reports"]} />
+              <Tags tags={[
+                "OOP",
+                "Delphi",
+                "SQL",
+                "Crystal Reports",
+              ]} />
             </Position>
 
           </section>
