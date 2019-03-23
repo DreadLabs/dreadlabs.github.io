d7f6aa36c9a5364c2934ac2b3f46d2454ff783d4
Thomas Juhnke <tommy@van-tomas.de>
2019-03-24 00:48:32 +0100

    Specify meta description

--- src/components/layout/metadata.js
+++ src/components/layout/metadata.js
@@ -3,7 +3,11 @@ import { Helmet } from "react-helmet"
 
 export default () => (
   <Helmet defaultTitle="Thomas Juhnke — DreadLabs Softwareentwicklung"
-          titleTemplate={`%s | Thomas Juhnke — DreadLabs Softwareentwicklung`}>
+          titleTemplate={`%s | Thomas Juhnke — DreadLabs Softwareentwicklung`}
+          meta={[
+            {name: 'description', content: 'Individuelle Softwareentwicklung, Qualitätssicherung und Beratung für kleine bis mittelgroße Softwareprojekte.'}
+          ]}
+  >
     <html lang="de" />
   </Helmet>
 )
